3822477e855873f1eb1d2f51efedd0eae4af79ef
Rakshita44 <rakshita.chormale@gmail.com>
2025-01-14 11:47:34 +0530

    Add files via upload

---
+++ EmployeeDetails.java
@@ -0,0 +1,61 @@
+package ANP;
+import java.util.Scanner;
+
+public class EmployeeDetails {
+
+	public static void main(String[] args) {
+		// Create a Scanner object to take input from the user
+		Scanner scanner= new Scanner(System.in);
+		// Declare variables to store employee information
+		int employeeId;
+		String employeeName;
+		String employeeAddress;
+		double employeeSalary;
+		
+		System.out.println("Enter Employee ID");
+		employeeId= scanner.nextInt();
+		scanner.nextLine();
+		
+		System.out.println("Enter Employee Name");
+		employeeName= scanner.nextLine();
+		
+		System.out.println("Enter Employee Address");
+		employeeAddress= scanner.nextLine();
+		
+		System.out.println("Enter Employee Salary");
+		employeeSalary= scanner.nextDouble();
+		
+		// Display the employee details
+		System.out.println("\nEmployee Deatils");
+		System.out.println("Employee ID:"+employeeId);
+		System.out.println("Employee Name:"+employeeName);
+		System.out.println("Employee Address:"+employeeAddress);
+		System.out.println("Employee Salary:"+employeeSalary);
+		
+		scanner.close();
+		
+	}
+
+}
+/*Output:
+ Enter Employee ID
+101
+Enter Employee Name
+rakshita
+Enter Employee Address
+Solapur
+Enter Employee Salary
+1200
+
+Employee Deatils
+Employee ID:101
+Employee Name:rakshita
+Employee Address:Solapur
+Employee Salary:1200.0
+*/
+
+
+
+
+
+
